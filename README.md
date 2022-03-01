7f77ddd200fa40087de0ce71f89541dc224addab
xiaojie.chen <xiaojie.chen@dianping.com>
2022-03-01 23:11:37 +0800

    to python3

--- README.md
+++ README.md
@@ -1,38 +1,4 @@
-# jsontolua
-a python script convert json to lua table
-
-#usage:
-git clone https://github.com/neoliang/jsontolua.git
-
-cd jsontolua
-
-python ./setup.py install
-
-## Quick Expamples
-
-#command line
-
-python ./convert_json.py json_file lua_file
-
-#code
-```python
-
-#convert string
-jsontolua.str_to_lua_table('{"a":"1","b":2,"c":[1,2,3]}')
-#return lua code 
-'''
-{
-	a = '1',
-	c = {
-		1,2,3
-	},
-	b = 2
-}
-
-'''
-#convert file
-jsontolua.file_to_lua_file('./1.json','./1.lua')
-
-```
-
+###python3 jsontolua.py jsonfile luafile
+### jsonfile 为输入json文件名, luafile 为输出的lua文件名
+### demo：python3 jsontolua.py 1.json 1.lua
 
--- jsontolua.py
+++ jsontolua.py
@@ -1,82 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import json
-import types
-import json
-import os
+import sys
 
 def space_str(layer):
-	spaces = ""
-	for i in range(0,layer):
-		spaces += '\t'
-	return spaces
+    spaces = ""
+    for i in range(0, layer):
+        spaces += '\t'
+    return spaces
 
-def dic_to_lua_str(data,layer=0):
+def dic_to_lua_str(data, layer=0):
+    d_type = type(data)
+    if d_type is str:
+        yield ("\"" + data + "\"")
+    elif d_type is bool:
+        if data:
+            yield ('true')
+        else:
+            yield ('false')
+    elif d_type is int or d_type is float:
+        yield (str(data))
+    elif d_type is list:
+        yield ("{\n")
+        yield (space_str(layer + 1))
+        for i in range(0, len(data)):
+            for sub in dic_to_lua_str(data[i], layer + 1):
+                yield sub
+            if i < len(data) - 1:
+                yield (',')
+        yield ('\n')
+        yield (space_str(layer))
+        yield ('}')
+    elif d_type is dict:
+        yield ("\n")
+        yield (space_str(layer))
+        yield ("{\n")
+        data_len = len(data)
+        data_count = 0
+        for k, v in data.items():
+            data_count += 1
+            yield (space_str(layer + 1))
+            if type(k) is int:
+                yield ('[' + str(k) + ']')
+            else:
+                yield ('[' + str(k) + ']')
+                #yield (k)
+            yield (' = ')
+            try:
+                for sub in dic_to_lua_str(v, layer + 1):
+                    yield sub
+                if data_count < data_len:
+                    yield (',\n')
 
-	d_type = type(data)
-	if  d_type is types.StringTypes or d_type is str or d_type is types.UnicodeType:
-		yield ("'" + data + "'")
-	elif d_type is types.BooleanType:
-		if data:
-			yield ('true')
-		else:
-			yield ('false')
-	elif d_type is types.IntType or d_type is types.LongType or d_type is types.FloatType:
-		yield (str(data))
-	elif d_type is types.ListType:
-		yield ("{\n")
-		yield (space_str(layer+1))
-		for i in range(0,len(data)):
-			for sub in  dic_to_lua_str(data[i],layer+1):
-				yield sub
-			if i < len(data)-1:
-				yield (',')
-		yield ('\n')
-		yield (space_str(layer))
-		yield ('}')
-	elif d_type is types.DictType:
-		yield ("\n")
-		yield (space_str(layer))
-		yield ("{\n")
-		data_len = len(data)
-		data_count = 0
-		for k,v in data.items():
-			data_count += 1
-			yield (space_str(layer+1))
-			if type(k) is types.IntType:
-				yield ('[' + str(k) + ']')
-			else:
-				yield (k) 
-			yield (' = ')
-			try:
-				for sub in  dic_to_lua_str(v,layer +1):
-					yield sub
-				if data_count < data_len:
-					yield (',\n')
+            except Exception as e:
+                print('error in ', k, v)
+                raise
+        yield ('\n')
+        yield (space_str(layer))
+        yield ('}')
+    else:
+        raise (d_type, 'is error')
 
-			except Exception, e:
-				print 'error in ',k,v
-				raise
-		yield ('\n')
-		yield (space_str(layer))
-		yield ('}')
-	else:
-		raise d_type , 'is error'
 
 def str_to_lua_table(jsonStr):
-	data_dic = None
-	try:
-		data_dic = json.loads(jsonStr)
-	except Exception, e:
-		data_dic =[]
-	else:
-		pass
-	finally:
-		pass
-	bytes = ''
-	for it in dic_to_lua_str(data_dic):
-		bytes += it
-	return bytes
-def file_to_lua_file(jsonFile,luaFile):
-	with open(luaFile,'w') as luafile:
-		with open(jsonFile) as jsonfile:
-			luafile.write(str_to_lua_table(jsonfile.read()))
+    data_dic = None
+    try:
+        data_dic = json.loads(jsonStr)
+    except Exception as e:
+        data_dic = []
+    else:
+        pass
+    finally:
+        pass
+    bytes = ''
+    for it in dic_to_lua_str(data_dic):
+        bytes += it
+    return bytes
+
+
+def file_to_lua_file(jsonFile, luaFile):
+    with open(luaFile, 'w', encoding='utf-8') as luafile:
+        with open(jsonFile) as jsonfile:
+            luafile.write(str_to_lua_table(jsonfile.read()))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("请输入两个参数：jsonfile（输入文件名）、luafile（输出文件名）")
+    jsonFile = sys.argv[1]
+    luaFile = sys.argv[2]
+    file_to_lua_file(jsonFile, luaFile)
