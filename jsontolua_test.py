bc2062a3766cbd9cf8b09c2c3e128e2c1c9e4671
xiaojie.chen <xiaojie.chen@dianping.com>
2022-03-02 01:02:37 +0800

    update

--- jsontolua_test.py
+++ jsontolua_test.py
@@ -3,6 +3,17 @@
 
 import jsontolua
 
+print('{"a":"1","b":2,"c":[1,2,3]}')
+print('-' * 6)
 print(jsontolua.str_to_lua_table('{"a":"1","b":2,"c":[1,2,3]}'))
+print('#' * 100)
+
+print('[]')
+print('-' * 6)
 print(jsontolua.str_to_lua_table('[]'))
+print('#' * 100)
+
+print('[1,2,3]')
+print('-' * 6)
 print(jsontolua.str_to_lua_table('[1,2,3]'))
+print('#' * 100)
